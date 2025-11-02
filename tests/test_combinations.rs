91203edd67306282a3ec5d78e0f52bbf2673314d
Henrik Kvamme <henrik@kvamme.xyz>
2025-11-02 03:53:16 +0100

    Fix straight and flush using 7 cards.

--- tests/test_combinations.rs
+++ tests/test_combinations.rs
@@ -149,38 +149,38 @@ fn run_test(setup: GameSetup, cards: Cards, expectation: Expectation) {
 }
 
 #[test]
-fn test_straight_flush_vs_quads() {
+fn test_straight_flush_vs_flush() {
     run_test(
         GameSetup {
             game_id: 1,
             players_out: 0,
-            players_folded: 4,
-            initial_chips: (0, 0, 100),
-            initial_bets: (100, 100, 0),
+            players_folded: 0,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
         },
         Cards {
             p1_cards: [card("S9"), card("S6")],
-            p2_cards: [card("H5"), card("D5")],
-            p3_cards: [card("H2"), card("C2")],
+            p2_cards: [card("SQ"), card("SK")],
+            p3_cards: [card("SA"), card("S2")],
             flop: [card("S8"), card("S7"), card("S5")],
-            turn: card("C5"),
+            turn: card("S3"),
             river: card("H10"),
         },
         Expectation {
-            winner_chips: Some((200, 0, 100)),
+            winner_chips: Some((300, 0, 0)),
         },
     );
 }
 
 #[test]
-fn test_four_of_kind_vs_full_house() {
+fn test_four_of_a_kind_vs_full_house() {
     run_test(
         GameSetup {
             game_id: 2,
             players_out: 0,
-            players_folded: 4,
-            initial_chips: (100, 0, 100),
-            initial_bets: (50, 50, 0),
+            players_folded: 0,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
         },
         Cards {
             p1_cards: [card("SA"), card("HA")],
@@ -191,7 +191,7 @@ fn test_four_of_kind_vs_full_house() {
             river: card("DQ"),
         },
         Expectation {
-            winner_chips: Some((200, 0, 100)),
+            winner_chips: Some((300, 0, 0)),
         },
     );
 }
@@ -202,9 +202,9 @@ fn test_flush_vs_straight() {
         GameSetup {
             game_id: 3,
             players_out: 0,
-            players_folded: 4,
-            initial_chips: (100, 0, 100),
-            initial_bets: (50, 50, 0),
+            players_folded: 0,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
         },
         Cards {
             p1_cards: [card("SA"), card("SK")],
@@ -215,31 +215,31 @@ fn test_flush_vs_straight() {
             river: card("H6"),
         },
         Expectation {
-            winner_chips: Some((200, 0, 100)),
+            winner_chips: Some((300, 0, 0)),
         },
     );
 }
 
 #[test]
-fn test_three_of_kind_vs_two_pair() {
+fn test_three_of_a_kind_vs_two_pair() {
     run_test(
         GameSetup {
             game_id: 4,
             players_out: 0,
-            players_folded: 4,
-            initial_chips: (100, 0, 100),
-            initial_bets: (50, 50, 0),
+            players_folded: 0,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
         },
         Cards {
             p1_cards: [card("SK"), card("HK")],
-            p2_cards: [card("SA"), card("HA")],
+            p2_cards: [card("SQ"), card("HA")],
             p3_cards: [card("H2"), card("H3")],
-            flop: [card("DK"), card("SQ"), card("HQ")],
+            flop: [card("DK"), card("SA"), card("HQ")],
             turn: card("S10"),
             river: card("S9"),
         },
         Expectation {
-            winner_chips: Some((200, 0, 100)),
+            winner_chips: Some((300, 0, 0)),
         },
     );
 }
@@ -250,9 +250,9 @@ fn test_pair_vs_high_card() {
         GameSetup {
             game_id: 5,
             players_out: 0,
-            players_folded: 4,
-            initial_chips: (100, 0, 100),
-            initial_bets: (50, 50, 0),
+            players_folded: 0,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
         },
         Cards {
             p1_cards: [card("SK"), card("HK")],
@@ -263,7 +263,7 @@ fn test_pair_vs_high_card() {
             river: card("C4"),
         },
         Expectation {
-            winner_chips: Some((200, 0, 100)),
+            winner_chips: Some((300, 0, 0)),
         },
     );
 }
@@ -274,41 +274,41 @@ fn test_same_pair_higher_kicker() {
         GameSetup {
             game_id: 6,
             players_out: 0,
-            players_folded: 4,
-            initial_chips: (100, 0, 100),
-            initial_bets: (50, 50, 0),
+            players_folded: 0,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
         },
         Cards {
             p1_cards: [card("S10"), card("SA")],
             p2_cards: [card("D10"), card("SK")],
-            p3_cards: [card("H2"), card("H3")],
+            p3_cards: [card("C10"), card("HK")],
             flop: [card("H10"), card("S9"), card("D7")],
             turn: card("S5"),
             river: card("D4"),
         },
         Expectation {
-            winner_chips: Some((200, 0, 100)),
+            winner_chips: Some((300, 0, 0)),
         },
     );
 }
 
 #[test]
-fn test_wheel_straight() {
+fn test_three_of_a_kind_kicker_comparison() {
     run_test(
         GameSetup {
             game_id: 7,
             players_out: 0,
             players_folded: 4,
-            initial_chips: (100, 0, 100),
-            initial_bets: (50, 50, 0),
+            initial_chips: (0, 0, 100),
+            initial_bets: (100, 100, 0),
         },
         Cards {
-            p1_cards: [card("SA"), card("H2")],
-            p2_cards: [card("D6"), card("H6")],
-            p3_cards: [card("H3"), card("H4")],
-            flop: [card("D3"), card("C4"), card("S5")],
-            turn: card("C9"),
-            river: card("C8"),
+            p1_cards: [card("SK"), card("HQ")],
+            p2_cards: [card("DK"), card("HJ")],
+            p3_cards: [card("H2"), card("H3")],
+            flop: [card("CK"), card("SA"), card("D9")],
+            turn: card("C7"),
+            river: card("C4"),
         },
         Expectation {
             winner_chips: Some((200, 0, 100)),
@@ -322,9 +322,9 @@ fn test_split_pot_playing_board() {
         GameSetup {
             game_id: 8,
             players_out: 0,
-            players_folded: 4,
-            initial_chips: (0, 0, 100),
-            initial_bets: (50, 50, 0),
+            players_folded: 0,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
         },
         Cards {
             p1_cards: [card("H2"), card("H3")],
@@ -335,7 +335,7 @@ fn test_split_pot_playing_board() {
             river: card("S10"),
         },
         Expectation {
-            winner_chips: Some((50, 50, 100)),
+            winner_chips: Some((100, 100, 100)),
         },
     );
 }
@@ -346,9 +346,9 @@ fn test_full_house_higher_trips() {
         GameSetup {
             game_id: 9,
             players_out: 0,
-            players_folded: 4,
-            initial_chips: (100, 0, 100),
-            initial_bets: (50, 50, 0),
+            players_folded: 0,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
         },
         Cards {
             p1_cards: [card("SA"), card("HA")],
@@ -359,7 +359,7 @@ fn test_full_house_higher_trips() {
             river: card("S9"),
         },
         Expectation {
-            winner_chips: Some((200, 0, 100)),
+            winner_chips: Some((300, 0, 0)),
         },
     );
 }
@@ -371,8 +371,8 @@ fn test_straight_higher_top_card() {
             game_id: 10,
             players_out: 0,
             players_folded: 4,
-            initial_chips: (100, 0, 100),
-            initial_bets: (50, 50, 0),
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
         },
         Cards {
             p1_cards: [card("S9"), card("H8")],
@@ -383,7 +383,198 @@ fn test_straight_higher_top_card() {
             river: card("CA"),
         },
         Expectation {
-            winner_chips: Some((200, 0, 100)),
+            winner_chips: Some((300, 0, 0)),
+        },
+    );
+}
+
+#[test]
+fn test_multiple_straights_board() {
+    run_test(
+        GameSetup {
+            game_id: 11,
+            players_out: 0,
+            players_folded: 4,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
+        },
+        Cards {
+            p1_cards: [card("SJ"), card("HQ")],
+            p2_cards: [card("D5"), card("H4")],
+            p3_cards: [card("H2"), card("C2")],
+            flop: [card("D6"), card("C7"), card("S8")],
+            turn: card("H9"),
+            river: card("C10"),
+        },
+        Expectation {
+            winner_chips: Some((300, 0, 0)),
+        },
+    );
+}
+
+#[test]
+fn test_full_house_higher_pair() {
+    run_test(
+        GameSetup {
+            game_id: 12,
+            players_out: 0,
+            players_folded: 0,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
+        },
+        Cards {
+            p1_cards: [card("SK"), card("HK")],
+            p2_cards: [card("SQ"), card("HQ")],
+            p3_cards: [card("H2"), card("H3")],
+            flop: [card("SA"), card("HA"), card("DA")],
+            turn: card("C9"),
+            river: card("C8"),
+        },
+        Expectation {
+            winner_chips: Some((300, 0, 0)),
+        },
+    );
+}
+
+#[test]
+fn test_wheel_vs_six_high_straight() {
+    run_test(
+        GameSetup {
+            game_id: 13,
+            players_out: 0,
+            players_folded: 0,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
+        },
+        Cards {
+            p1_cards: [card("SA"), card("H10")],
+            p2_cards: [card("D6"), card("H7")],
+            p3_cards: [card("C10"), card("CJ")],
+            flop: [card("D3"), card("C4"), card("S5")],
+            turn: card("H2"),
+            river: card("C8"),
+        },
+        Expectation {
+            winner_chips: Some((0, 300, 0)),
+        },
+    );
+}
+
+#[test]
+fn test_royal_flush_vs_lower_straight_flush() {
+    run_test(
+        GameSetup {
+            game_id: 14,
+            players_out: 0,
+            players_folded: 0,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
+        },
+        Cards {
+            p1_cards: [card("SA"), card("SK")],
+            p2_cards: [card("HK"), card("H9")],
+            p3_cards: [card("C2"), card("C3")],
+            flop: [card("SQ"), card("SJ"), card("S10")],
+            turn: card("HQ"),
+            river: card("HJ"),
+        },
+        Expectation {
+            winner_chips: Some((300, 0, 0)),
+        },
+    );
+}
+
+#[test]
+fn test_straight_flush_wheel() {
+    run_test(
+        GameSetup {
+            game_id: 15,
+            players_out: 0,
+            players_folded: 0,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
+        },
+        Cards {
+            p1_cards: [card("SA"), card("S2")],
+            p2_cards: [card("H6"), card("D6")],
+            p3_cards: [card("H7"), card("H8")],
+            flop: [card("S3"), card("S4"), card("S5")],
+            turn: card("C6"),
+            river: card("S6"),
+        },
+        Expectation {
+            winner_chips: Some((300, 0, 0)),
+        },
+    );
+}
+
+#[test]
+fn test_board_straight_different_pairs() {
+    run_test(
+        GameSetup {
+            game_id: 16,
+            players_out: 0,
+            players_folded: 4,
+            initial_chips: (0, 0, 50),
+            initial_bets: (100, 100, 50),
+        },
+        Cards {
+            p1_cards: [card("HA"), card("DA")],
+            p2_cards: [card("HK"), card("DK")],
+            p3_cards: [card("H2"), card("H3")],
+            flop: [card("C7"), card("D8"), card("S9")],
+            turn: card("H10"),
+            river: card("CJ"),
+        },
+        Expectation {
+            winner_chips: Some((125, 125, 50)),
+        },
+    );
+}
+
+#[test]
+fn test_two_pair_kicker() {
+    run_test(
+        GameSetup {
+            game_id: 17,
+            players_out: 0,
+            players_folded: 0,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
+        },
+        Cards {
+            p1_cards: [card("SA"), card("H3")],
+            p2_cards: [card("DJ"), card("H2")],
+            p3_cards: [card("C2"), card("C3")],
+            flop: [card("SK"), card("HK"), card("DQ")],
+            turn: card("CQ"),
+            river: card("H5"),
+        },
+        Expectation {
+            winner_chips: Some((300, 0, 0)),
+        },
+    );
+}
+#[test]
+fn test_flush_tie_same_top_five() {
+    run_test(
+        GameSetup {
+            game_id: 19,
+            players_out: 0,
+            players_folded: 0,
+            initial_chips: (0, 0, 0),
+            initial_bets: (100, 100, 100),
+        },
+        Cards {
+            p1_cards: [card("S8"), card("S7")],
+            p2_cards: [card("S6"), card("S5")],
+            p3_cards: [card("S2"), card("S3")],
+            flop: [card("SA"), card("SK"), card("SQ")],
+            turn: card("SJ"),
+            river: card("S9"),
+        },
+        Expectation {
+            winner_chips: Some((100, 100, 100)),
         },
     );
 }
